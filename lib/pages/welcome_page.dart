1e0ef2e2d70fe4801046e97925f5bcecd4f740e9
Toretay <abdymvzhit@gmail.com>
2025-04-17 10:49:52 +0500

    created icon and welcome text

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -7,6 +7,16 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Welcome Page"), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.abc, size: 120.0),
+            Text("Welcome to our app!", textAlign: TextAlign.center),
+          ],
+        ),
+      ),
     );
   }
 }
