7a34e529ab2e9d700f54c8e63c91181b63e9cb65
Toretay <abdymvzhit@gmail.com>
2025-04-17 10:51:57 +0500

    added safeArea for buttons to stay properly

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -7,19 +7,21 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Welcome Page"), centerTitle: true),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Spacer(),
-            Icon(Icons.abc, size: 120.0),
-            Text("Welcome to our app!", textAlign: TextAlign.center),
-            Spacer(),
-            ElevatedButton(onPressed: () {}, child: Text("Register")),
-            SizedBox(height: 6.0),
-            ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              Icon(Icons.abc, size: 120.0),
+              Text("Welcome to our app!", textAlign: TextAlign.center),
+              Spacer(),
+              ElevatedButton(onPressed: () {}, child: Text("Register")),
+              SizedBox(height: 6.0),
+              ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
+            ],
+          ),
         ),
       ),
     );
