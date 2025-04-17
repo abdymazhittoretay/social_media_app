ea1e214e372ba89c9e5cfeba8e97f617db382044
Toretay <abdymvzhit@gmail.com>
2025-04-17 16:13:31 +0500

    created textfields with text controllers

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,10 +8,46 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Register Page"), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: "Your email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                hintText: "Your password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(
+                hintText: "Confirm password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
