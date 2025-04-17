d29abd8e1ddd4b241993e61ed568508d031b73ec
Toretay <abdymvzhit@gmail.com>
2025-04-17 16:23:57 +0500

    added safeArea for proper alignment

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -17,48 +17,50 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Register Page"), centerTitle: true),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Spacer(),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  hintText: "Your email",
-                  border: OutlineInputBorder(),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Spacer(),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: "Your email",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  hintText: "Your password",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 12.0),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: "Your password",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(
-                  hintText: "Confirm password",
-                  border: OutlineInputBorder(),
+                SizedBox(height: 12.0),
+                TextField(
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(
+                    hintText: "Confirm password",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              Spacer(),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.maxFinite, 50),
-                  backgroundColor: Theme.of(context).primaryColor,
-                  foregroundColor: Theme.of(context).secondaryHeaderColor,
+                Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.maxFinite, 50),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Theme.of(context).secondaryHeaderColor,
+                  ),
+                  onPressed: () {},
+                  child: Text("Register"),
                 ),
-                onPressed: () {},
-                child: Text("Register"),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
