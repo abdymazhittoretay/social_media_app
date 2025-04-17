e60fa4ae8c22d7ec35de95ada02ce2a9a0637917
Toretay <abdymvzhit@gmail.com>
2025-04-17 10:53:03 +0500

    added vertical and horizontal padding

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -9,18 +9,24 @@ class WelcomePage extends StatelessWidget {
       appBar: AppBar(title: Text("Welcome Page"), centerTitle: true),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Spacer(),
-              Icon(Icons.abc, size: 120.0),
-              Text("Welcome to our app!", textAlign: TextAlign.center),
-              Spacer(),
-              ElevatedButton(onPressed: () {}, child: Text("Register")),
-              SizedBox(height: 6.0),
-              ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 12.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Spacer(),
+                Icon(Icons.abc, size: 120.0),
+                Text("Welcome to our app!", textAlign: TextAlign.center),
+                Spacer(),
+                ElevatedButton(onPressed: () {}, child: Text("Register")),
+                SizedBox(height: 6.0),
+                ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
+              ],
+            ),
           ),
         ),
       ),
