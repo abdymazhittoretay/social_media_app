9cce91780b3473d60b898c0746d3b31f8a2ddec5
Toretay <abdymvzhit@gmail.com>
2025-04-22 15:32:51 +0500

    added addPost method

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
+import 'package:social_media_app/models/post_model.dart';
 
 final ValueNotifier<FirestoreService> authService = ValueNotifier(
   FirestoreService(),
@@ -9,4 +10,13 @@ class FirestoreService {
   final CollectionReference _posts = FirebaseFirestore.instance.collection(
     "posts",
   );
+
+  Future<void> addPost(PostModel post) {
+    return _posts.add({
+      "username": post.username,
+      "content": post.content,
+      "likes": post.likes,
+      "timestamp": post.timestamp,
+    });
+  }
 }
