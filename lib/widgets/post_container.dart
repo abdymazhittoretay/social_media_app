93c0fd5f80e4d8f49285b36fe51c97454e461db3
Toretay <abdymvzhit@gmail.com>
2025-04-25 21:04:21 +0500

    fixed stack elements ovelapping with column

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -13,77 +13,72 @@ class PostContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(left: 12.0, right: 12.0, bottom: 12.0),
-      height: 100,
       decoration: BoxDecoration(
         color: Theme.of(context).primaryColor,
         borderRadius: BorderRadius.circular(12.0),
       ),
-      child: Stack(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.all(12.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.person, color: Colors.white),
-                  SizedBox(width: 6.0),
-                  Text(post.username, style: TextStyle(color: Colors.white)),
-                ],
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.person, color: Colors.white),
+                SizedBox(width: 6.0),
+                Text(post.username, style: TextStyle(color: Colors.white)),
+              ],
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(post.content, style: TextStyle(color: Colors.white)),
-            ),
+            child: Text(post.content, style: TextStyle(color: Colors.white)),
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    if (post.whoLiked.contains(
-                      authService.value.currentUser!.email,
-                    )) {
-                      firestoreService.value.removeLike(docID);
-                    } else {
-                      firestoreService.value.addLike(docID);
-                    }
-                  },
-                  icon: Icon(
-                    post.whoLiked.contains(authService.value.currentUser!.email)
-                        ? Icons.favorite
-                        : Icons.favorite_border,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      if (post.whoLiked.contains(
+                        authService.value.currentUser!.email,
+                      )) {
+                        firestoreService.value.removeLike(docID);
+                      } else {
+                        firestoreService.value.addLike(docID);
+                      }
+                    },
+                    icon: Icon(
+                      post.whoLiked.contains(
+                            authService.value.currentUser!.email,
+                          )
+                          ? Icons.favorite
+                          : Icons.favorite_border,
 
-                    color: Colors.white,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Text(
-                  post.likes.toString(),
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
+                  Text(
+                    post.likes.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              post.email == authService.value.currentUser!.email
+                  ? IconButton(
+                    onPressed: () async {
+                      await firestoreService.value.removePost(docID);
+                    },
+                    icon: Icon(Icons.delete, color: Colors.white),
+                  )
+                  : SizedBox(),
+            ],
           ),
-          post.email == authService.value.currentUser!.email
-              ? Align(
-                alignment: Alignment.bottomRight,
-                child: IconButton(
-                  onPressed: () async {
-                    await firestoreService.value.removePost(docID);
-                  },
-                  icon: Icon(Icons.delete, color: Colors.white),
-                ),
-              )
-              : SizedBox(),
         ],
       ),
     );
